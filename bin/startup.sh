8b6c6a48a39f0e9c877ae802d4011d80fab0d317
cdai <dc_726@163.com>
2016-10-05 20:28:28 -0400

    phase 2: route table initial

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,4 +1,4 @@
 
 make
 
-dar -f --logdir `pwd`/logs/ --logfilename master.`date +"%y%m%d-%H%M%S"`.log src/master.da
+dar -f --logdir `pwd`/logs/ --logfilename master.`date +"%y-%m-%d"`.log src/master.da
