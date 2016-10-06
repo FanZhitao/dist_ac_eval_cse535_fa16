45482579fae8ede5e3a244514dafaeee40cab478
cdai <dc_726@163.com>
2016-10-05 22:09:32 -0400

    phase 2: read config

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,4 +1,14 @@
+#!/bin/bash -x
+
+if [ $# -gt 1 ]; then
+    level="$1"
+    config="$2"
+else
+    level="info"
+    config="config/test1.config"
+fi
 
 make
 
-dar -f --logdir `pwd`/logs/ --logfilename master.`date +"%y-%m-%d"`.log src/master.da
+dar -L $level -F $level -f --logdir `pwd`/logs/ --logfilename master.`date +"%y-%m-%d"`.log src/master.da $config
+
